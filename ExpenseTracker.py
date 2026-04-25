46662b83d7e78fe2faf26b3832adf343b08bf419
Everson alijaya <eversonalijaya@Mac.lan>
2026-04-25 19:47:06 -0400

    feat: adds expenses by month option

--- ExpenseTracker.py
+++ ExpenseTracker.py
@@ -37,6 +37,20 @@ def view_by_category():
      for category, total in category_totals.items():
         print(f"{category.capitalize()}: ${total:.2f}")
 
+def expenses_by_month():
+    if len(expenses) == 0:
+        print ("No expenses recorded.")
+        return
+    for date, category, amount, description in expenses:
+        month = date[5:7]
+        if month in category_totals:
+            category_totals[month] += amount
+        else:
+            category_totals[month] = amount
+    print("\nTotal Expenses by Month:")
+    for month, total in category_totals.items():
+        print(f"{month}: ${total:.2f}")
+
 def main():
     print("Welcome to the Expense Tracker!")
     while True:
@@ -45,9 +59,10 @@ def main():
         print("2. View All Expenses")
         print("3. View By Category")
         print("4. View Total Expenses")
-        print("5. Exit")
+        print("5. Expenses by month")
+        print("6. Exit")
         
-        choice = input("Choose an option (1-5):").strip()
+        choice = input("Choose an option (1-6):").strip()
         
         if choice == '1':
             year = input("Enter the year of the expense (YYYY): ").strip()
@@ -73,6 +88,9 @@ def main():
             show_total()
 
         elif choice == '5':
+            expenses_by_month()
+
+        elif choice == '6':
             print("goodbye!")
             break
         
